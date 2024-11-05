6af3ee0c48136e9c683d71ad576d51ac7535c080
VVizard3618 <gthra36@proton.me>
2024-11-05 18:10:45 +0000

    mixed lists

---
+++ tuesdaychallenge.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+mylist = [1, 2, 3, 4, 5, "Python"]
+
+name = input("What is your name?\n> ")
+
+# Print a welcome message
+print(f"Hi {name.capitalize()}! Welcome to Day {mylist[1]} of {mylist[5]} Training!")
+
+
+
